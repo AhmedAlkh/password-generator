2317c91f65374444d87043c351104340f599efa3
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-11-02 21:27:07 -0400

    password criteria prompts added

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,30 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  
+  // Prompt user to select password criteria they would like to have included
+  // Choose password length (8-128 characters)
+  var passwordLength = window.prompt("Choose password length (8-128 characters)");
+
+    if (passwordLength >= 8 && passwordLength <= 128)  {
+      window.alert("Your password is " + passwordLength + " characters long.");
+    }
+      else {
+      window.alert("Please try again.");
+    }
+
+  // Choose password characters. Lowercase, uppercase, numbers, special characters.
+  var lowerCase = window.confirm("Would you like to include lowercase letters?");
+  var upperCase = window.confirm("Would you like to include uppercase letters?");
+  var numberic = window.confirm("Would you like to include numbers?");
+  var specialCharacters = window.confirm("Would you like to include special characters?");
+}
+
+  // Input should be validated 
+
+  // Display password to the page
+  //return password;
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Password Generator Starter Code
+
+## Updates
+- Added function for already existing generatePassword variable
+- Added password length selection prompt that is functioning
+- Added password character prompts that appear but do not yet function. (uppercase,lowercase,numbers, special characters)
+
+## To Do
+- Add validation messages for the 4 character type prompts
+- Add in all character types
+- Add "Password is being generated" message when all password criteria has been selected
+- Generate "return" password to text box
+
