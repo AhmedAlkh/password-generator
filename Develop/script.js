c391e5e1dffe55189db211fafd3e3a6b9fa4b1ba
AhmedA <alkhatib_ahmed1995@hotmail.com>
2021-11-07 17:52:47 -0500

    push and publish page

--- Develop/script.js
+++ Develop/script.js
@@ -1,24 +1,33 @@
-// Assignment code here
+// Global variables
+
 var generateBtn = document.querySelector("#generate");
 var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numberCharacters = "01234567890";
 var specialCharacters = "!'#$%&()*+-./:;<=>?@[]^_`{|},~";
-
 var includeLowerCase;
 var includeUpperCase;
 var includeNumbers;
 var includeSymbols;
 
+// Global variables ^
+
+// Main function
+
 function generatePassword() {
   var password = "";
   var newPassword = "";
+
+
+  // User is prompted to select password length. If too short or long they are prompted to try again.
 
   var getPasswordLength = window.prompt("Choose the length of your password(8-128 characters).");
   while ((getPasswordLength < 8) || (getPasswordLength > 128)) {
     window.alert("Please try again.");
     getPasswordLength = window.prompt("Choose the length of your password(8-128 characters).");
   }
+
+  // This function gathers all of the password criteria.
 
   function passwordCriteria() {
 
@@ -54,12 +63,17 @@ function generatePassword() {
     else {
       window.alert("Your password WILL NOT include special characters.");
     }
+
+    // User must select atleast one character type or the loop repeats.
+
     while ((!includeLowerCase) && (!includeUpperCase) && (!includeNumbers) && (!includeSymbols)) {
       window.alert("You must select at least 1 character type.");
       passwordCriteria();
     }
   }
   passwordCriteria();
+
+  // A password consisting of the selected length and criteria is randomly generated.
 
   for (var i = 0; i < getPasswordLength; i++) {
     password += newPassword.charAt(Math.floor(Math.random() * newPassword.length));
@@ -68,6 +82,8 @@ function generatePassword() {
   return password;
 }
 
+// Main function ^
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -80,89 +96,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Password should consist of all user input combined. password length + password criteria
-// generate random characters of password at the length decided by user
-// math.floor, math.random, .charAt
-
-// old code - first attempt
-// Assignment code here
-/*
-var generateBtn = document.querySelector("#generate");
-var finalPassword = password;
-var passwordCriteria = passwordLength + lowerCaseLetters + upperCaseLetters + numberCharacters + specialCharacters;
-var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numberCharacters = "01234567890";
-var specialCharacters = "!'#$%&()*+-./:;<=>?@[]^_`{|},~";
-var passwordLength = choosePasswordLength
-
-function generatePassword() {
-
-  // Prompt user to select password criteria they would like to have included
-  // Choose password length (8-128 characters)
-  var choosePasswordLength = window.prompt("Choose password length (8-128 characters)");
-  console.log(choosePasswordLength);
-    if (choosePasswordLength >= 8 && choosePasswordLength <= 128)  {
-      window.alert("Your password is " + choosePasswordLength + " characters long.");
-    }
-      else {
-      window.alert("Please try again.");
-    }
-
-  // Choose password characters. Lowercase, uppercase, numbers, special characters.
-  // Input should be validated
-
-
-
-if (passwordCriteria = passwordLength + includeLowerCase + includeUpperCase + includeNumbers + includeSymbols) {
-  finalPassword = lowerCaseLetters + upperCaseLetters + numberCharacters + lowerCaseLetters
-  console.log(finalPassword)
-}
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// Display password to the page
-
-function passwordCriteria() {
-var choosePasswordCriteria = window.prompt("Choose which types of characters you'd like to include in your password. 1 = lower case letters, 2 = upper case letters, 3 = numbers, 4 = symbols (example: for all character types, enter '1 2 3 4')");
-  choosePasswordCriteria = parseInt(choosePasswordCriteria);
-
-  switch (choosePasswordCriteria) {
-    case 1:
-      lowerCaseLetters;
-      break;
-    case 2:
-      upperCaseLetters;
-      break;
-    case 3:
-      numberCharacters;
-      break;
-    case 4:
-      symbolCharacters;
-      break;
-    default:
-      window.alert("Please try again.")
-      break;
-  }
-}
-
-README
-
-description
-
-screenshot
-
-github repo
-
-deployed app
-
-*/
--- README.md
+++ README.md
@@ -1,14 +1,12 @@
 # Password Generator
 
-## Updates
-- COMPLETE - If windows alert asks user to please try again, they should be asked to input info again rather than carry on.
-- COMPLETE - Link the character types so that if they are confirmed to be used, they are included in the final password.
-- COMPLETE - Final password is sent to text box
+## Description
+1. User clicks the red "Generate Password" button to begin.
+2. User is prompted to select the password length. Length must be between 8 to 128 characters. If password length is too short or long, user cannot continue and is prompted to try again.
+3. User is prompted to select password criteria (lower case letters, upper case letters, numbers, special characters). If user decides to use or not to use a character type, they are alerted that their password WILL or WILL NOT contain that character type. User must select at least one character type or they cannot continue and are alerted to select atleast one character type.
+4. Password consisting of the length and criteria selected by the user is randomly generated and sent to the web page's main text box. It can now be copied and pasted!
 
-## To Do
-- Add comments
-- Clean up script
-- Publish web page
-- Update readme
-- Submit
+## Screenshots
+COMING SOON!!!
+
 
